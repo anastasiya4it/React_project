5314673286dfaf5a8d56b3b56089b1d03876079e
Bo136 <anastasiya4it@gmail.com>
2024-08-29 05:31:33 +0300

    page error

--- src/pages/ErrorPage/ui/ErrorPage.tsx
+++ src/pages/ErrorPage/ui/ErrorPage.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/className/className";
+import * as cls from "./ErrorPage.module.scss";
+import errorImage from "shared/assets/images/404.png";
 
 const ErrorPage = () => {
   const { t } = useTranslation();
   return (
-    <div>
-      <h1>{t("Error 404")}</h1>
-      <h2>{t("Page not found")}</h2>
+    <div className={classNames(cls.main, {}, [cls.overlay])}>
+      <div className={classNames(cls.text)}>
+        <h1>{t("404")}</h1>
+        <h2>{t("Sorry, we couldn`t find this page")}</h2>
+      </div>
+      <img src={errorImage} alt="404" className={classNames(cls.img)} />
     </div>
   );
 };
--- src/shared/config/routerConfig/RouterConfig.tsx
+++ src/shared/config/routerConfig/RouterConfig.tsx
@@ -4,27 +4,27 @@ import { MainPage } from "pages/MainPage";
 import { PathRouteProps, type RouteProps } from "react-router-dom";
 
 export enum Routes {
-	main = "main",
-	about = "about",
-	error = "error",
+  main = "main",
+  about = "about",
+  error = "error",
 }
 
 export const RouterPath: Record<Routes, string> = {
-	[Routes.main]: "/",
-	[Routes.about]: "/about",
-	[Routes.error]: "/:pathMatch(.*)/",
+  [Routes.main]: "/",
+  [Routes.about]: "/about",
+  [Routes.error]: "/*",
 };
 export const RouterConfig: ReadonlyArray<RouteProps> = [
-	{
-		path: RouterPath.main,
-		element: <MainPage />,
-	},
-	{
-		path: RouterPath.about,
-		element: <AboutPage />,
-	},
-	{
-		path: RouterPath.error,
-		element: <ErrorPage />,
-	},
+  {
+    path: RouterPath.main,
+    element: <MainPage />,
+  },
+  {
+    path: RouterPath.about,
+    element: <AboutPage />,
+  },
+  {
+    path: RouterPath.error,
+    element: <ErrorPage />,
+  },
 ];
