3fe76773a36c36f802549e1881c2ea71355338c3
Bo136 <anastasiya4it@gmail.com>
2024-08-31 17:28:00 +0300

    add loader

--- src/app/provider/RouterProvader/ui/AppRouter.tsx
+++ src/app/provider/RouterProvader/ui/AppRouter.tsx
@@ -1,23 +1,24 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { RouterConfig } from "shared/config/routerConfig/RouterConfig";
+import { Loader } from "shared/ui/Loader";
 
 const AppRouter = () => {
-	return (
-		<div className="app">
-			<Suspense fallback={<div>Завантаження...</div>}>
-				<Routes>
-					{RouterConfig.map((element) => (
-						<Route
-							key={element.path}
-							path={element.path}
-							element={element.element}
-						/>
-					))}
-				</Routes>
-			</Suspense>
-		</div>
-	);
+  return (
+    <div className="app">
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {RouterConfig.map((element) => (
+            <Route
+              key={element.path}
+              path={element.path}
+              element={element.element}
+            />
+          ))}
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default AppRouter;
---
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -0,0 +1,52 @@
+import { classNames } from "shared/lib/className/className";
+import * as cls from "./Loader.module.scss";
+import { useState, useRef, useEffect } from "react";
+import { useTheme } from "app/provider/ThemeProvider/lib/useTheme";
+interface LoaderProps {
+  className?: string;
+  // startx: number;
+  // starty: number;
+}
+
+const Loader = ({ className }: LoaderProps) => {
+  const widthMax = "100%";
+  const heightMax = "100%";
+  const s = window.screen;
+  const width = s.width;
+  const height = s.availHeight;
+  // console.log(width, height, s);
+  const { theme } = useTheme();
+  const fillColor =
+    theme === "dark" ? "rgba(9,9,73,.2)" : "rgba(232,232,234,.2)";
+  const letter = Array.from({ length: 256 }, () => {
+    return 65 + Math.floor(Math.random() * 50);
+  });
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const draw = () => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+      // console.log(context);
+
+      if (context) {
+        context.fillStyle = fillColor;
+        context.fillRect(0, 0, width, height);
+        context.fillStyle = "#0F0";
+        letter.map((yPos, index) => {
+          const text = String.fromCharCode(65 + Math.random() * 50);
+          const xPos = index * 10;
+          context.fillText(text, xPos, yPos);
+          letter[index] = yPos > 758 + Math.random() * 1e4 ? 0 : yPos + 10;
+        });
+      }
+    }
+  };
+  setInterval(draw, 50);
+  return (
+    <canvas ref={canvasRef} height={height} className={classNames(cls.main)} />
+  );
+};
+
+export { Loader };
