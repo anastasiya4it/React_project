73d5dd2d21b1ffb3ee5ad592340a6a9aac0ee0a2
Bo136 <anastasiya4it@gmail.com>
2024-09-10 22:04:10 +0300

    fix

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,6 +1,6 @@
-import { ResolveOptions } from "webpack";
-import path from "path";
-import { BuildOptions } from "./types/config";
+import type { ResolveOptions } from "webpack";
+import path from "node:path";
+import type { BuildOptions } from "./types/config";
 
 export function buildResolvers({ paths }: BuildOptions): ResolveOptions {
   return {
